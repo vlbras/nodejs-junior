063ebf60a26d76cab0130003a067df8165d2dde2
vlad <vlad.bras02@gmail.com>
2023-06-06 13:34:20 +0200

    feat: add CRUD routes for Customer

--- src/customer/customer.resolver.ts
+++ src/customer/customer.resolver.ts
@@ -1,7 +1,8 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Customer } from 'lib/entities/customer.entity';
 import { CustomerService } from './customer.service';
 import { GetCustomerInput } from './dto/customer.input';
+import { updateCustomerInput } from './dto/update-customer.input';
 
 @Resolver(() => Customer)
 export class CustomerResolver {
@@ -10,5 +11,26 @@ export class CustomerResolver {
   @Query(() => [Customer])
   async customers(@Args('data') { skip, take, where }: GetCustomerInput) {
     return this.customerService.findAll({ skip, take, where });
+  }
+
+  // find Customer by id or email
+  @Query(() => Customer)
+  async customer(@Args('identifier') identifier: string): Promise<Customer>{
+    return this.customerService.findOne(identifier)
+  }
+
+  // update Customer by id or email
+  @Mutation(() => Customer)
+  async updateCustomer(
+    @Args('identifier') identifier: string,
+    @Args('updateCustomerInput') updateCustomerInput: updateCustomerInput
+    ): Promise<Customer> {
+    return this.customerService.update(identifier, updateCustomerInput)
+  }
+
+  // delete Customer by id or email
+  @Mutation(() => Customer)
+  async deleteCustomer(@Args('identifier') identifier: string): Promise<Customer> {
+    return this.customerService.delete(identifier)
   }
 }
