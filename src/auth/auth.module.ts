90da44b85e600493093162f589cddfc7b3e65aa6
vlad <vlad.bras02@gmail.com>
2023-06-06 13:54:53 +0200

    feat: use JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_ACCESS_TOKEN_TTL },
+    }),],
   providers: [AuthResolver, AuthService]
 })
 export class AuthModule {}
